49b2e25858ef767467c49f9ed5a39a8ac1005f83
Adam Plaver <adam.plaver@gmail.com>
2015-02-11 11:29:49 -0500

    Started pulse animation

--- neopixel.ino
+++ neopixel.ino
@@ -10,16 +10,25 @@ void setup() {
 }
 
 void loop() {
-  fade(pixel.color(255.85,0), pixel.color(255, 25, 0), 100);
+  fadeLoop(pixel.color(255, 85,0), pixel.color(255, 25, 0), 100);
+  pulse(pixel.color(255, 25, 0), pixel.setBrightness(50), 500);
 }
 
-void fade(uint32_t color1, uint32_t color2, uint8_t wait){ // Fades between two colors
-    for(int i=0; i<NUMPIXELS; i++){
+void fadeLoop(uint32_t color1, uint32_t color2, uint8_t wait){ // Fades between two colors
+  for(int i=0; i<NUMPIXELS; i++){
     pixels.setPixelColor(i, color1);
     pixels.show();
     delay(wait);
     pixels.setPixelColor(i, color2);
     delay(wait)
     pixels.show();
+  }
+}
+
+void pusle(uint32_t color, uint8_t minB, wait){
+  for(int i=0; i<NUMPIXELS; i++){
+    pixels.setPixelColor(i, color);
+    pixels.show();
+    delay(wait);
   }
 }
