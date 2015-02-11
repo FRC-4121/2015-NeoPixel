26aea9b60b74c029e3d1d68a1c420a0a907b50a1
Adam Plaver <adam.plaver@gmail.com>
2015-02-11 11:20:45 -0500

    Added basic loop
    Fades between two colors

---
+++ neopixel.ino
@@ -0,0 +1,25 @@
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+#define PIN            6
+#define NUMPIXELS      60
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() {
+  fade(pixel.color(255.85,0), pixel.color(255, 25, 0), 100);
+}
+
+void fade(uint32_t color1, uint32_t color2, uint8_t wait){ // Fades between two colors
+    for(int i=0; i<NUMPIXELS; i++){
+    pixels.setPixelColor(i, color1);
+    pixels.show();
+    delay(wait);
+    pixels.setPixelColor(i, color2);
+    delay(wait)
+    pixels.show();
+  }
+}
