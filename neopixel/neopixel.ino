5b6063a01c54103ec368b4837229a8ee48dbaf65
Adam Plaver <adam.plaver@gmail.com>
2015-02-11 15:34:49 -0500

    Possibly working fadeLoop

--- neopixel/neopixel.ino
+++ neopixel/neopixel.ino
@@ -12,18 +12,21 @@ void setup() {
 
 void loop() {
   fadeLoop(strip.Color(255, 85,0), strip.Color(255, 25, 0), 100);
-  pulse(strip.Color(255, 25, 0), 50, 500);
+  // pulse(strip.Color(255, 25, 0), 50, 500);
 }
 
 void fadeLoop(uint32_t color1, uint32_t color2, uint8_t wait){ // Fades between two colors
-  for(int i=0; i<strip.numPixels(); i++){
-    strip.setPixelColor(i, color1);
-    strip.show();
-    delay(wait);
-    strip.setPixelColor(i, color2);
-    delay(wait);
-    strip.show();
-  }
+	uint8_t i, c;
+	for(int c=0; c<5; c++){ // Loop count
+	  for(int i=0; i<strip.numPixels(); i++){ // Sets color
+	    strip.setPixelColor(i, color1);
+	    strip.show();
+	    delay(wait);
+	  }
+	  strip.setPixelColor(i, color2);
+	  delay(wait);
+	  strip.show();
+	}
 }
 
 void pulse(uint32_t color, uint8_t minB, uint8_t wait){
