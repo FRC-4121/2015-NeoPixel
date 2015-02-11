aade56f7ca133e0687365d0d7b381d12d3ed81e5
Adam Plaver <adam.plaver@gmail.com>
2015-02-11 17:02:46 -0500

    Working fade cycle

--- neopixel/neopixel.ino
+++ neopixel/neopixel.ino
@@ -7,26 +7,42 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   strip.begin(); // This initializes the NeoPixel library.
+  startUp();
   strip.show();
 }
 
 void loop() {
-  fadeLoop(strip.Color(255, 85,0), strip.Color(255, 25, 0), 100);
+  fadeLoop(strip.Color(255, 85,0), strip.Color(0, 255, 0), 10);
   // pulse(strip.Color(255, 25, 0), 50, 500);
+}
+
+void startUp(){ // Flashes green to show it works
+	uint8_t i;
+	for(int i=0; i<strip.numPixels(); i++){
+		strip.setPixelColor(i, 0, 255, 0);
+		strip.show();
+		delay(1);
+	}
+	delay(500);
+	for(int i=0; i<strip.numPixels(); i++){
+		strip.setPixelColor(i, 0, 0, 0);
+		strip.show();
+		delay(1);
+	}
 }
 
 void fadeLoop(uint32_t color1, uint32_t color2, uint8_t wait){ // Fades between two colors
 	uint8_t i, c;
-	for(int c=0; c<5; c++){ // Loop count
-	  for(int i=0; i<strip.numPixels(); i++){ // Sets color
+	for(int c=0; c< 255 * 5; c++){
+  	for(int i=0; i<strip.numPixels(); i++){ // Sets color
 	    strip.setPixelColor(i, color1);
+	    strip.setBrightness(Fade(c));
 	    strip.show();
 	    delay(wait);
-	  }
-	  strip.setPixelColor(i, color2);
-	  delay(wait);
-	  strip.show();
-	}
+  	}
+	 	delay(10);
+	 	strip.show();
+  }
 }
 
 void pulse(uint32_t color, uint8_t minB, uint8_t wait){
@@ -35,4 +51,16 @@ void pulse(uint32_t color, uint8_t minB, uint8_t wait){
     strip.show();
     delay(wait);
   }
+}
+
+uint32_t Fade(byte FadePos){
+	if(FadePos < 150){
+		return(FadePos * 25);
+	} else if(150 < FadePos < 200 ) {
+		return(FadePos * 15);
+	} else if(200 < FadePos < 255) {
+		return(FadePos / 15);
+	} else {
+		return(25);
+	}
 }
