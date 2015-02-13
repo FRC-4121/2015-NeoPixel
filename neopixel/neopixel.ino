129fa4eb5e98827e229ac998c7895b277f037a53
Adam Plaver <adam.plaver@gmail.com>
2015-02-13 16:47:32 -0500

    Working Strobe
    Still can't figure out how to fade between colors, seems like it's going
    to be a lot of math.

--- neopixel/neopixel.ino
+++ neopixel/neopixel.ino
@@ -11,9 +11,9 @@ void setup() {
 }
 
 void loop() {
-  fadeLoop(255, 80, 0, 255, 150, 0, 10, 10); 										// Fades between two oranges
-  chaseLoop(strip.color(255,0,0), strip.color(0,0,255), 100); 	// Has a single pixel changing color down the line.
-  fadeLoop(255, 255, 255, 255, 0, 0, 10, 25);
+  // fadeLoop(10); 																							// Fades between two oranges
+  chaseLoop(strip.Color(0,0,0), strip.Color(255,80,0), 10); 	// Has a single pixel changing color down the line.
+  // rainbowCycle(50);
 }
 
 void startUp(){ // Flashes green to show it's on
@@ -31,31 +31,47 @@ void startUp(){ // Flashes green to show it's on
 	}
 }
 
-void fadeLoop(uint8_t Rstart, uint8_t Gstart, uint8_t Bstart, uint8_t Rend, uint8_t Gend, uint8_t Bend, uint8_t n, uint8_t wait){
-	// Fades between two colors. Args: Begining RGB vals, Ending RGB vals, steps, and delay time.
-	uint8_t i, c;
-	for(int c=0; c< 5; c++){ // loop count
-		for(int i = 0; i < n; i++){ // larger values of 'n' will give a smoother/slower transition.
-		  Rnew = Rstart + (Rend - Rstart) * i / n;
-		  Gnew = Gstart + (Gend - Gstart) * i / n;
-		  Bnew = Bstart + (Bend - Bstart) * i / n;
-			strip.setPixelColor(i, Rnew, Gnew, Bnew);
-			strip.show();
-			delay(wait);
+void fadeLoop(uint8_t wait) {
+	uint8_t i, j, c;
+	//for(int c = 0; c < 5; c++){
+		for(int j = 80; j < 256; j++) {
+		  for(int i=0; i<strip.numPixels(); i++) {
+		    strip.setPixelColor(i, strip.Color(255 , j, 0));
+		    strip.show();
+		    delay(wait);
+		  }
 		}
-	}
+		for(int j = 255; j > 0; j--) {
+		  for(int i=0; i<strip.numPixels(); i++) {
+		    strip.setPixelColor(i, strip.Color(255, j, 0));
+		    strip.show();
+		    delay(wait);
+		  }
+		}
+	//}
+}
+
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
 }
 
 void chaseLoop(uint32_t color1, uint32_t color2, uint8_t wait){
 	uint8_t i, c;
-	for(int c=0; c < 5; c++){ // Loop count
+	for(int c=0; c < 10; c++){ // Loop count
 		for(int i=0; i < strip.numPixels(); i++){
-			strip.setPixelColor(i, color1); // Sets the pixel color to the background
-			strip.setPixelColor((i - 1), color1);
-			strip.show();
-			delay(1);
-			strip.setPixelColor(i, color2); // Sets the chase pixel color.
-			strip.setPixelColor((i - 1), (color2 / 2));
+			strip.setPixelColor(i, color1); // Sets the chase pixel color.
+			strip.setPixelColor(i + 1, color1);
+			strip.setPixelColor(i + 2, color1);
+			strip.setPixelColor(i - 1, color2); // Sets the pixel color to the background
+			strip.setPixelColor(i - 2, color2);
 			strip.show();
 			delay(wait);
 		}
